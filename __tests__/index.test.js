80bf23a9ad87e2bee59a6ad1140b1497199c6e83
den <denisamigous@gmail.com>
2020-11-15 18:06:28 +0100

    parsing html with inherit

---
+++ __tests__/index.test.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+import parse from '../index.js';
+
+describe('HtmlBuilder', () => {
+  test('#toString', () => {
+    const data = ['html', [
+      ['head', [
+        ['title', 'hello, hexlet!'],
+      ]],
+      ['body', [
+        ['div', { class: 'separator' }],
+        ['h1', { class: 'header' }, 'html builder example'],
+        ['div', [
+          ['img', { class: 'image', src: '#' }],
+          ['span', 'span text2'],
+        ]],
+      ]],
+    ]];
+
+    const ast = parse(data);
+    const result = '<html><head><title>hello, hexlet!</title></head><body><div class="separator"></div><h1 class="header">html builder example</h1><div><img class="image" src="#"><span>span text2</span></div></body></html>';
+    expect(ast.toString()).toEqual(result);
+  });
+});
